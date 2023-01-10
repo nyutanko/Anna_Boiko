442c382d1883a706b58474fd0d9f10ae4e423b5a
nyutanko <82411488+nyutanko@users.noreply.github.com>
2023-01-10 13:16:26 +0200

    Added scenario

---
+++ features/scraper.feature
@@ -0,0 +1,7 @@
+Feature testing OrangeHRMScraper 
+
+    Scenario: test scrapper
+    Given started the driver
+    When we loginned
+    When new job added
+    Then job removed
--- features/scraper.py
+++ features/scraper.py
@@ -1,1 +1,23 @@
+from behave import *
+from ..main import OrangeHRMScraper
+
+
+@given('started the driver')
+def init_driver(context):
+    OrangeHRMScraper.__init__()
+
+
+@when('we loginned')
+def login(context):
+    OrangeHRMScraper.login()
+
+
+@when('new job added')
+def add_new_job(context):
+    OrangeHRMScraper.add_new_job()
+
+
+@then('job removed')
+def remove_job(context):
+    OrangeHRMScraper.remove_job()
 
